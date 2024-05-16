ea5615daae2df15965ff8277c0a335f9274a9372
danilgutak <otecgutak@gmail.com>
2024-05-16 15:03:21 +0200

    gotovee nahuyy

--- srcs/requirements/wordpress/wp-init.sh
+++ srcs/requirements/wordpress/wp-init.sh
@@ -8,9 +8,11 @@ chmod +x wp-cli.phar
 
 
 ./wp-cli.phar core download --allow-root
+
 ./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PW --dbhost=mariadb --allow-root --extra-php <<PHP
 define('FS_METHOD', 'direct');
 PHP
+
 ./wp-cli.phar core install --url=localhost --title="incpetion by dgutak" --admin_user=$DB_USER \
  --admin_password=$DB_USER_PW --admin_email=$WP_MAIL --allow-root
 chmod -R 777 /var/www/html
